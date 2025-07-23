5cb722f93face42d4650348381ba9433a4aa5721
Leonie Miege <leonie.miege@gmail.com>
2025-07-23 15:26:29 +0200

    fix : test register

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,12 +3,15 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
     public function test_register(): void
     {
-        $response = $this->postJson('/api/users', [
+        $response = $this->postJson('/api/users/register', [
             'display_name' => 'Lolo le zozo',
             'username' => 'nadrojria',
             'email' => 'miso_maven@umamifarm.net',
@@ -18,19 +21,20 @@ class UserTest extends TestCase
         );
 
         $response
-
             ->assertStatus(200)
-
-            ->assertJson([
-                'created' => true,
-            ]);
-
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->whereAllType([
+            'token' => 'string',
+            'Type' => 'string'
+        ])
+);
     }
-    public function test_authenticate_user(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)
-            ->assertTrue($user->isAuthenticate());
     }
-}
+//     public function test_authenticate_user(): void
+//     {
+//         $user = User::factory()->create();
+
+//         $response = $this->actingAs($user)
+//             ->assertTrue($user->isAuthenticate());
+//     }
+// }
