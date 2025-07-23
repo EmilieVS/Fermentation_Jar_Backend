2826c341662ee59394b54f9c47f4d9d62b9aa6d5
AxelleBdd <axbaudinaud@gmail.com>
2025-07-23 11:04:27 +0200

    Update: Updating the first tests

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -7,22 +7,23 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase
 {
     public function test_register(): void
-
     {
-
         $response = $this->postJson('/api/users', ['name' => 'Sally']);
-
- 
 
         $response
 
             ->assertStatus(200)
 
             ->assertJson([
-
                 'created' => true,
-
             ]);
 
+    }
+    public function test_authenticate_user(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->assertTrue($user->isAuthenticate());
     }
 }
