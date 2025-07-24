6d0d337cd8ef68a517cb20175ca1cf86ad0d6405
AxelleBdd <axbaudinaud@gmail.com>
2025-07-24 11:24:05 +0200

    Add: Creating test for editProfile methode

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -35,39 +35,28 @@ class UserTest extends TestCase
             );
     }
 
-    // public function test_getUserData(): void
-    // {
-    //     $response = $this->postJson(
-    //         '/api/users',
-    //         [
-    //             'display_name' => 'Lolo le zozo',
-    //             'username' => 'nadrojria',
-    //             'email' => 'miso_maven@umamifarm.net',
-    //             'password' => 'pickle123',
-    //             'created_at' => now(),
-    //             'updated_at' => now()
-    //         ]
-    //     );
-
-    //     $response
-    //         ->assertStatus(200)
-    //         ->assertJson(
-    //             fn(AssertableJson $json) =>
-    //             $json->whereAllType([
-    //                 'token' => 'string',
-    //                 'Type' => 'string'
-    //             ])
-    //         );
-    // }
-
     public function test_getUserData(): void
     {
         $user = User::factory()->create();
- 
+        
         $response = $this->actingAs($user)  
             ->withSession(['banned' => false])
             ->get('api/users')
             ->assertStatus(200);
-
+        
     }
-}
+    
+    public function test_editProfile(): void
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)
+            ->withSession(['banned' => false])
+            ->put('api/users', [
+                "display_name"=> "FinalTest",
+                "email"=> "miso_test@umamifarm.net",
+                "password"=> "confirmedTest"
+            ])
+            ->assertStatus(200);
+        }
+    }
