d234842362b205bc4116f305a60a02363330786b
AxelleBdd <axbaudinaud@gmail.com>
2025-07-29 11:53:21 +0200

    Update: New test and one needing a code modification to pass

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Testing\TestResponse;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Testing\TestResponseAssert as PHPUnit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
 use App\Models\Post;
@@ -32,37 +34,33 @@ class PostTest extends TestCase
                 ])
             );
         }
-        
-        // Test case 2 : Unsucessful post if there are less than 20 characters
-        public function test_post_fails_if_not_enougth_characters(): void
-        {
-            $user = User::factory()->create();
-    
-            $response = $this->actingAs($user)
-                ->withSession(['banned' => false])
-                ->post('/api/post',
-                [
-                    'description' => 'Trop court.',
-                ])
-                ->assertStatus(404);            
+
+    // Test case 2 : The user can access their posts
+    public function test_getUserPost(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['banned' => false])
+            ->get('/api/user/post')
+            ->assertStatus(200);
     }
-
-
-    // Test case 3 : Unsucessful post if there are more than 300 characters
-    // public function test_post_fails_if_too_much_characters(): void
-    // {
-       
-    // }
-
-    // Test case 4 : The user can access their posts
-    // public function test_getUserPost(): void
-    // {
-        
-    // }
     
-    // Test case 5 : The user can see all posts
-    // public function test_getAllPosts(): void
-    // {
-        
-    // }
+    // Test case 3 : The user can see all posts unauthenticated
+    public function test_getAllPosts(): void
+    {
+        $response = $this->getJson('/api/post');
+
+        $response->assertStatus(200)
+             ->assertJsonStructure([ // To use assertJsonStructure() we have to get the Json first
+                 'post' => [
+                     '*' => [
+                         'description',
+                         'display_name',
+                         'username',
+                         'created_at'
+                     ]
+                 ]
+             ]);
     }
+}
